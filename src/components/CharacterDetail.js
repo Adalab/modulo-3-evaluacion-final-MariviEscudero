b73a1d062abe912a6c6ac47c071b27cd976b4b2a
MariviEscudero <mariviesmo@gmail.com>
2021-10-04 13:11:48 +0200

    Add className to characterdetail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,35 +1,38 @@
 import { Link } from 'react-router-dom';
 const CharacterDetail = (props) => {
-  const statusIcon =() =>{
-    if(props.detailData.status === 'Alive'){
-      return(<spam>&#128522;</spam>);
-    }else{
-      return(<spam>&#9760;</spam>);
+  const statusIcon = () => {
+    if (props.detailData.status === 'Alive') {
+      return <spam>&#128522;</spam>;
+    } else {
+      return <spam>&#9760;</spam>;
     }
-  }
+  };
   return (
     <section className="main__section--detail">
-      <Link to='/'>
-        <i className="fas fa-backspace icon"></i>
+      <Link to="/">
+        <i class="fas fa-times-circle icon"></i>
       </Link>
-
+      <h2 className="main__section--detail__name">{props.detailData.name}</h2>
       <img
         className="main__section--detail__image"
         src={props.detailData.image}
         alt={props.detailData.name}
       />
-     
+
       <ul>
-        <li className="main__section--detail__name">{props.detailData.name}</li>
-        <li className="main__section--detail__specie">
+       
+        <li className="main__section--detail__data">
           Especie: {props.detailData.specie}
         </li>
-        <li className="main__section--detail__origin">
+        <li className="main__section--detail__data">
           Planeta de origen: {props.detailData.origin}
         </li>
-        <li>Aparece en la serie en {props.detailData.episodes} episodios</li>
-        <li>Estado en este universo: {statusIcon()}</li>
-        
+        <li className="main__section--detail__data">
+          Aparece en la serie en {props.detailData.episodes} episodios
+        </li>
+        <li className="main__section--detail__data">
+          Estado en este universo: {statusIcon()}
+        </li>
       </ul>
     </section>
   );
