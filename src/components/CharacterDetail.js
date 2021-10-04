95face65410f022146da4e671d3dcba1d935384f
MariviEscudero <mariviesmo@gmail.com>
2021-10-04 16:39:02 +0200

    Fix specie icons

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,18 +1,26 @@
 import { Link } from 'react-router-dom';
 const CharacterDetail = (props) => {
   const statusIcon = () => {
-    if (props.detailData.status === 'Alive') {
-      return <spam>&#128522;</spam>;
+    if (props.detailData.status === 'Dead') {
+      return <spam>&#128128;</spam>;
     } else {
-      return <spam>&#9760;</spam>;
+      return "Sigue vivo";
     }
   };
+  const specieIcon = () => {
+    if (props.detailData.specie === 'Alien') {
+      return <spam>&#128126;</spam>;
+    } else {
+      return "Humano o clon humano, quién sabe ya...";
+    }
+  };
+
   return (
     <section className="main__section--detail">
       <Link to="/">
         <i class="fas fa-times-circle icon"></i>
       </Link>
-     
+
       <img
         className="main__section--detail__image"
         src={props.detailData.image}
@@ -20,9 +28,9 @@ const CharacterDetail = (props) => {
       />
 
       <ul className="main__section--detail__list">
-      <li className="main__section--detail__name">{props.detailData.name}</li>
+        <li className="main__section--detail__name">{props.detailData.name}</li>
         <li className="main__section--detail__data">
-          Especie: {props.detailData.specie}
+          Especie: {specieIcon()}
         </li>
         <li className="main__section--detail__data">
           Planeta de origen: {props.detailData.origin}
--- src/components/PageError.js
+++ src/components/PageError.js
@@ -1,16 +1,16 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../styles/PageError.scss';
-const PageError = (props)=>{
-return(
+const PageError = () => {
+  return (
     <section className="main__errorpage">
-    <p>
-      Malditos agujeros espacio-temporales! la página que buscas no
-      existe en este universo...Prueba con otra dirección o ve a inicio
-      <Link to='/'>
-      <span className="main__errorpage--span"> aquí</span>
-      </Link>
-    </p>
-  </section>
-);
-}
+      <p>
+        Wubba Lubba Dub Dub!!! la página que buscas no existe en este
+        universo...Prueba con otra dirección o ve a inicio
+        <Link to="/">
+          <span className="main__errorpage--span"> aquí</span>
+        </Link>
+      </p>
+    </section>
+  );
+};
 export default PageError;
